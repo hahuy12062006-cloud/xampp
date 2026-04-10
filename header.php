4f0d8de4d85b9dbba4f26debb3568fe50ec004d4
Your Name <you@example.com>
2026-04-10 16:33:24 +0700

    add

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Huy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="CSS/header.css">
+<body>
+<img src="IMG/them.webp"width="100%">
+<div id="header">
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+<div class ="icon-bar">
+      <input type="text" placeholder="Search..">
+</div>
+</div>
+</div>
+</body>
+</html>
